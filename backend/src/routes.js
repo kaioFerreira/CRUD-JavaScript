1cb1e94b3459e8385ffa9951e48267b1d38ba68d
kaioFerreira <kaiofhs@hotmail.com>
2020-04-05 21:49:11 -0300

    Subindo a Aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const RegistroControler = require('./controllers/RegistroController');
+
+const routes = express.Router();
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Metodos HTTP:
+  * 
+  * GET: Busca/listar um informação do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Altera uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+/**
+ * Tipos de parâmentros
+ * 
+ * Query Params, Route Params, Request Body
+ */
+
+/**
+ *  Query: Parámetros nomeados e enviados na rota após "?" (Filtros, paginação)
+ * 
+ *  /users?nome=Kaio&idade=55
+ *  const { nome , idade } = request.query;
+ */
+
+/**
+ * Route: Parametros utilizados para identificar recursos
+ * 
+ * /users/50
+ * const { id } = request.params;
+*/
+
+/**
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * const { nome, idade } = request.body;
+*/
+
+routes.get('/registros', RegistroControler.index);
+routes.post('/registros', RegistroControler.store);
+routes.delete('/registro/:id', RegistroControler.delete);
+
+module.exports = routes;
